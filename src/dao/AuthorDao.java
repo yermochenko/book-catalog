c064d40dfb4b0fa86b6132ab1cc792c09b191685
Сергей Александрович Ермоченко <yermochenko@tut.by>
2020-11-27 00:45:49 +0300

    Structure of DAO class for author created

---
+++ src/dao/AuthorDao.java
@@ -0,0 +1,30 @@
+package dao;
+
+import java.util.List;
+
+import domain.Author;
+
+public class AuthorDao {
+	public Long create(Author author) {
+		// INSERT
+		return null;
+	}
+
+	public Author read(Long id) {
+		// SELECT
+		return null;
+	}
+
+	public List<Author> readAll() {
+		// SELECT
+		return null;
+	}
+
+	public void update(Author author) {
+		// UPDATE
+	}
+
+	public void delete(Long id) {
+		// DELETE
+	}
+}
